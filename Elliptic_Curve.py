d7bba4fd629e99fe293d6690c60908f508ce9df6
Jugurtha <jugurtha.2009@gmail.com>
2023-10-20 23:05:37 +0200

    Implemented a simple EC and it'sbasic operation

---
+++ Elliptic_Curve.py
@@ -0,0 +1,61 @@
+INFINITY = None
+
+
+class EllipticCurve:
+    def __init__(self, a, b, p, G, n, h):
+        self.a = a
+        self.b = b
+        self.p = p
+        self.G = G
+        self.n = n
+        self.h = h
+
+    def add(self, P, Q):
+        if P == INFINITY:
+            return Q
+        if Q == INFINITY:
+            return P
+
+        (x1, y1) = P
+        (x2, y2) = Q
+
+        # If we add points symmetrical with respect to Y-axis
+        if self.equal_modp(x1, x2) and self.equal_modp(y1, -y2):
+            return INFINITY
+
+        # Calculation slope for:
+        # Point doubling
+        if self.equal_modp(x1, x2) and self.equal_modp(y1, y2):
+            u = self.reduce_modp((3 * x1 * x1 + self.a) * self.inverse_modp(2 * y1))
+        # Point addition
+        else:
+            u = self.reduce_modp((y1 - y2) * self.inverse_modp(x1 - x2))
+
+        v = self.reduce_modp(y1 - u * x1)
+        x3 = self.reduce_modp(u * u - x1 - x2)
+        y3 = self.reduce_modp(-u * x3 - v)
+        return x3, y3
+
+    def multiply(self, k, P):
+        # Double and add algorithm from least significant bit to most significant bit
+        Q = INFINITY
+        while k != 0:
+            if k & 1 != 0:
+                Q = self.add(Q, P)
+            P = self.add(P, P)
+            k >>= 1
+        return Q
+
+    def reduce_modp(self, x):
+        return x % self.p
+
+    def equal_modp(self, x, y):
+        return self.reduce_modp(x - y) == 0
+
+    def inverse_modp(self, x):
+        if self.reduce_modp(x) == 0:
+            return None
+        return pow(x, self.p - 2, self.p)#From Fermat's therorem supposing p is prime
+
+    def is_point_on_curve(self, x, y):
+        return self.equal_modp(y * y, x * x * x + self.a * x + self.b)
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from tinyec.ec import SubGroup, Curve
+import Elliptic_Curve as EC
+
 
 def generate_curve(a, b, p):
     curve = {
-        'X' : [],
-        'Y' : []
+        'X': [],
+        'Y': []
     }
     for x in range(p):
         for y in range(p):
-            if (( y**2 - (x**3 + a*x + b) ) % p) == 0:
+            if ((y ** 2 - (x ** 3 + a * x + b)) % p) == 0:
                 curve['X'].append(x)
                 curve['Y'].append(y)
     return curve
@@ -28,3 +30,27 @@ if __name__ == '__main__':
     for k in range(0, 25):
         p = k * curve.g
         print(f"{k} * G = ({p.x}, {p.y})")
+
+    p = 26959946667150639794667015087019630673557916260026308143510066298881
+    a = -3
+    b = 18958286285566608000408668544493926415504680968679321075787234672564
+
+    Gx = 19277929113566293071110308034699488026831934219452440156649784352033
+    Gy = 19926808758034470970197974370888749184205991990603949537637343198772
+    G = (Gx, Gy)
+    n = 26959946667150639794667015087019625940457807714424391721682722368061
+    h = 1
+    P224 = EC.EllipticCurve(a, b, p, G, n, h)
+    # as specified in https://doi.org/10.6028/NIST.SP.800-186
+    print("\n-------------------------------------------\n")
+    print(P224.is_point_on_curve(Gx, Gy))# Verify G in curve
+
+    Q = P224.multiply(1, G)# Verify 1*G in curve
+    print(Q == G)
+
+    Q = P224.multiply(n - 1, G)# Verify (n-1)G in curve
+    print(Q)
+    print(P224.is_point_on_curve(Q[0], Q[1]))
+
+    Q = P224.multiply(n, G)# Verify nG is Infinity point
+    print(Q == EC.INFINITY)
